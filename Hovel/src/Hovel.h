39897ef65c73c43922e11a00edc986189a021109
s1c_b0y <m.spandel86@gmail.com>
2021-01-04 20:38:56 +0100

    Added keycodes, shifted file Input.h

--- Hovel/src/Hovel.h
+++ Hovel/src/Hovel.h
@@ -2,10 +2,14 @@
 // Only for Hovel application
 
 #include "Hovel/Application.h"
+
 #include "Hovel/Layer.h"
 #include "Hovel/Log.h"
-
 #include "Hovel/ImGui/ImGuiLayer.h"
+
+#include "Hovel/Input.h"
+#include "Hovel/KeyCodes.h"
+#include "Hovel/MouseButtonCodes.h"
 
 // ----- Entry Point -------------
 #include "Hovel/EntryPoint.h"
---
+++ Hovel/src/Hovel/MouseButtonCodes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// From glfw3.h
+
+#define HV_MOUSE_BUTTON_1         0
+#define HV_MOUSE_BUTTON_2         1
+#define HV_MOUSE_BUTTON_3         2
+#define HV_MOUSE_BUTTON_4         3
+#define HV_MOUSE_BUTTON_5         4
+#define HV_MOUSE_BUTTON_6         5
+#define HV_MOUSE_BUTTON_7         6
+#define HV_MOUSE_BUTTON_8         7
+#define HV_MOUSE_BUTTON_LAST      HV_MOUSE_BUTTON_8
+#define HV_MOUSE_BUTTON_LEFT      HV_MOUSE_BUTTON_1
+#define HV_MOUSE_BUTTON_RIGHT     HV_MOUSE_BUTTON_2
+#define HV_MOUSE_BUTTON_MIDDLE    HV_MOUSE_BUTTON_3
