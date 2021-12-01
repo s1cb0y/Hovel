9559cbc67f5d7195f5b082021e0fbef93f7a31eb
s1c_b0y <m.spandel86@gmail.com>
2021-12-01 21:47:11 +0100

    Added missing changes from previous commit

--- Hovel/src/Hovel/Application.h
+++ Hovel/src/Hovel/Application.h
@@ -34,7 +34,7 @@ namespace Hovel {
 		bool OnWindowClose(WindowCloseEvent& e);
 		bool SetCameraOrientation(KeyPressedEvent& e);
 		bool m_Running = true;
-		std::unique_ptr<Window> m_Window;
+		Scope<Window> m_Window;
 		ImGuiLayer* m_ImGuiLayer;
 		LayerStack m_layerStack;
 
